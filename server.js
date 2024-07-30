fd10b64496c077a4b8c621acf300428376a4be70
ScottKinnear <gr8whtninjassk@gmail.com>
2024-07-29 20:35:37 -0700

    All methods except delete work from the browser, Delete works in Postman

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express =  require('express')
 const app = express()
 const mongoose = require('mongoose')
 const MONGO_URI = process.env.MONGO_URI
-const Note = require('./models/car')
 const logger = require('morgan')
 const Car = require('./models/car')
 const PORT = 3000
@@ -25,14 +24,17 @@ mongoose.connection.on('error', () => {
 
 //CONTROLLER AND ROUTER LOGIG
 
+
+
+
 //CREATE
 app.post('/cars', async (req, res) => {
     req.body.isElectric === 'on' || req.body.isElectric === true? 
     req.body.isElectric = true : 
     req.body.isElectric= false
     try {
-        const createdCar =  await Note.create(req.body)
-        res.status(301).redirect(`/notes/${createdCar._id}`)
+        const createdCar =  await Car.create(req.body)
+        res.status(301).redirect(`/cars/${createdCar._id}`)
     } catch (error) {
         console.error(error)
         res.status(400).json({ message: error.message })
@@ -40,32 +42,83 @@ app.post('/cars', async (req, res) => {
     }
 })
 
-app.get('/notes/new', (req, res) => {
+app.get('/cars/new', (req, res) => {
     res.render('new.ejs')
 })
 
 //READ
-
+//Index
 app.get('/cars', async (req, res) => {
     try {
         const foundCars = await Car.find({})
         res.render('index.ejs', {
-            notes: foundCars
+            cars: foundCars
         })
     } catch (error) {
         res.status(400).json({ msg: error.message })
     }
 })
 
+//Show
+app.get('/cars/:id', async (req, res) => {
+    try {
+        const foundCar = await Car.findOne({ _id: req.params.id})
+        res.render('show.ejs', {
+            car: foundCar
+        })
+    } catch (error) {
+        res.status(400).json({ msg: error.message })  
+    }
+})
 
-//UPDAAATE
 
 
-//SHOW
+//UPDATE
+
+app.get('/cars/:id/edit', async (req, res) => {
+    const foundCar = await Car.findOne({ _id: req.params.id})
+    res.render('edit.ejs', {
+        car: foundCar
+    })
+})
+
+app.post('/cars/:id', async (req, res) => {
+    req.body.isElectric === 'on' || req.body.isElectric === true? 
+    req.body.isElectric = true : 
+    req.body.isElectric= false
+    try {
+        const updatedCar = await Car.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })// new: true sends the updated version
+        res.status(301).redirect(`/cars/${updatedCar._id}`)
+    } catch (error) {
+        res.status(400).json({ msg: error.message})
+    }
+})
+
 
 
 //DELETE
 
+app.delete('/cars/:id', async (req, res) => {
+    try {
+        await Car.findOneAndDelete({ _id: req.params.id })
+        .then((car) => {
+            res.sendstatus(204)
+        })        
+    } catch (error) {
+        res.status(400).json({ msg: error.message })
+    }
+})
+
+app.get('/cars/:id', async (req, res) => {
+    try {
+        const foundCar = await Car.findOne({ _id: req.params.id})
+        res.render('show.ejs', {
+            car: foundCar
+        })
+    } catch (error) {
+        res.status(400).json({ msg: error.message })  
+    }
+})
 
 
 
