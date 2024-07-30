0c7c58264af209e9f7556d88a953630f2c5a5df0
ScottKinnear <gr8whtninjassk@gmail.com>
2024-07-29 18:32:07 -0700

    views complete, create and index routes created

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const express =  require('express')
 const app = express()
 const mongoose = require('mongoose')
 const MONGO_URI = process.env.MONGO_URI
-const Note = require('./models/note')
+const Note = require('./models/car')
 const logger = require('morgan')
+const Car = require('./models/car')
 const PORT = 3000
 
 app.use(express.json())
@@ -25,8 +26,36 @@ mongoose.connection.on('error', () => {
 //CONTROLLER AND ROUTER LOGIG
 
 //CREATE
+app.post('/cars', async (req, res) => {
+    req.body.isElectric === 'on' || req.body.isElectric === true? 
+    req.body.isElectric = true : 
+    req.body.isElectric= false
+    try {
+        const createdCar =  await Note.create(req.body)
+        res.status(301).redirect(`/notes/${createdCar._id}`)
+    } catch (error) {
+        console.error(error)
+        res.status(400).json({ message: error.message })
+        
+    }
+})
+
+app.get('/notes/new', (req, res) => {
+    res.render('new.ejs')
+})
 
 //READ
+
+app.get('/cars', async (req, res) => {
+    try {
+        const foundCars = await Car.find({})
+        res.render('index.ejs', {
+            notes: foundCars
+        })
+    } catch (error) {
+        res.status(400).json({ msg: error.message })
+    }
+})
 
 
 //UPDAAATE
@@ -36,3 +65,9 @@ mongoose.connection.on('error', () => {
 
 
 //DELETE
+
+
+
+
+
+app.listen(PORT, () => { console.log('I see connected apps' + ` application accepting requests on PORT ${PORT}`) })
